09f7f99b44343525d10fc2ee56e1927589bc428b
David Emory <demory@Davids-MacBook-Pro.local>
2016-02-09 21:51:26 -0500

    Navbar layout

--- app/navbar.js
+++ app/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap'
 
 export default class NavigationBar extends React.Component {
 
@@ -22,15 +22,19 @@ export default class NavigationBar extends React.Component {
     }
 
     return (
-      <Navbar inverse>
+      <Navbar>
         <Navbar.Header>
           <Navbar.Brand>
             <a href="#">{this.props.title}</a>
           </Navbar.Brand>
         </Navbar.Header>
+        <Nav>
+          <NavItem href="http://localhost:9000">Manager</NavItem>
+          <NavItem href="http://localhost:9001">Editor</NavItem>
+          <NavItem href="#" active>Users</NavItem>
+        </Nav>
         <Nav pullRight>
-          <NavItem href="#">Admin</NavItem>
-          <NavItem href="#">Documentation</NavItem>
+          <NavItem href="#"><Glyphicon glyph="question-sign" /> Guide</NavItem>
           {userControl}
         </Nav>
       </Navbar>
